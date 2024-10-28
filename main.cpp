90e78b64ec30bad1b276a289e2d578f048da8080
ryuto-alt <k023g0078@g.neec.ac.jp>
2024-10-28 13:42:50 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,37 @@
+﻿#include<Windows.h>
+#ifdef _DEBUG
+#include<iostream>
+#endif
+
+using namespace std;
+
+///@brief コンソール画面にフォーマット付き文字列を表示
+///@param format フォーマット(%dとか%fとかの)
+///@param 可変長引数
+///@remarksこの関数はデバッグ用です。デバッグ時にしか動作しません
+void DebugOutputFormatString(const char* format, ...) {
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	printf(format, valist);
+	va_end(valist);
+
+#endif // DEBUG
+}
+
+#ifdef _DEBUG
+
+int main() {
+#else
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	DebugOutputFormatString("Show window test.");
+	getchar();
+	return 0;
+}
+#endif // DEBUG
+DebugOutputFormatString("show window test.");
+getchar();
+return 0;
+}
+
