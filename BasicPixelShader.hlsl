505650bdf301bbb0796455e5d0260f8aa4a6dca9
ryuto-alt <k023g0078@g.neec.ac.jp>
2024-12-04 15:25:27 +0900

    7.12.1から

--- BasicPixelShader.hlsl
+++ BasicPixelShader.hlsl
@@ -4,7 +4,6 @@ float4 BasicPS(Output input) : SV_TARGET
 {
     float3 light = normalize(float3(1, -1, 1));
     float brightness = dot(-light, input.normal);
-    return float4(brightness, brightness, brightness, 1)*diffuse;
-	//return float4(input.normal.xyz,1);
-	//return float4(tex.Sample(smp,input.uv));
+
+    return float4(brightness,brightness,brightness,1); // RGB+Alpha
 }
