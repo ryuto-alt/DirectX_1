9d2531836c06bcf5e02d8e2823c5d62dc2e02d67
ryuto-alt <146914455+ryuto-alt@users.noreply.github.com>
2025-04-09 13:31:29 +0900

    なおした

--- BasicVertexShader.hlsl
+++ BasicVertexShader.hlsl
@@ -3,7 +3,6 @@
 Output BasicVS(float3 pos : POSITION, float2 uv : TEXCOORD)
 {
     Output output;
-    // ここを変更：行列を左側に置いて mul(mat, float4(...)) とする
     output.svpos = mul(mat, float4(pos, 1.0f));
     output.uv = uv;
     return output;
